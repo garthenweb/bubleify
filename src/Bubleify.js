a5630c0afad33d25141dddf38c62422a77b9c1fb
Jannick Garthen <jannick.garthen@gmail.com>
2016-04-23 18:54:01 +0200

    Add options and sourcemap support

--- src/Bubleify.js
+++ src/Bubleify.js
@@ -2,10 +2,23 @@ import buble from 'buble';
 import { Transform } from 'stream';
 
 class Bubleify extends Transform {
-  constructor(filename, ...args) {
-    super(filename, ...args);
+  constructor(filename, options) {
+    super();
     this._data = '';
     this._filename = filename;
+    this._options = options;
+  }
+
+  get _bubleOptions() {
+    const defaults = { source: this._filename };
+    const options = Object.assign(defaults, this._options);
+    // set default transforms with deactivated modules
+    options.transforms = Object.assign({ modules: false }, options.transforms);
+
+    // remove browserify options
+    delete options._flags;
+
+    return options;
   }
 
   _transform(buf, enc, callback) {
@@ -15,9 +28,16 @@ class Bubleify extends Transform {
 
   _flush(callback) {
     try {
-      const result = buble.transform(this._data);
+      const result = buble.transform(this._data, this._bubleOptions);
+      let { code } = result;
+
+      // append sourcemaps to code
+      if (this._options._flags.debug) {
+        code += `\n//# sourceMappingURL=${result.map.toUrl()}`;
+      }
+
       this.emit('bubleify', result, this._filename);
-      this.push(result.code);
+      this.push(code);
     } catch (err) {
       this.emit('error', err);
       return;
@@ -26,4 +46,4 @@ class Bubleify extends Transform {
   }
 }
 
-export default (filename) => new Bubleify(filename);
+export default (filename, options) => new Bubleify(filename, options);
--- tests/browserify-test.js
+++ tests/browserify-test.js
@@ -2,7 +2,10 @@ const path = require('path');
 const browserify = require('browserify');
 const bubleify = require('../index');
 
-const b = browserify();
+const b = browserify({ debug: true });
 b.add(path.join(__dirname, './test-file.js'));
-b.transform(bubleify);
+b.transform(bubleify, {
+  target: { chrome: 50 },
+  transforms: { arrow: true },
+});
 module.exports = b.bundle();
