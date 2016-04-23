84b78897f4070c637867c7f7dface135108defb3
Jannick Garthen <jannick.garthen@gmail.com>
2016-04-24 00:44:42 +0200

    Remove further es2015 from tests

--- tests/build.test.js
+++ tests/build.test.js
@@ -22,7 +22,7 @@ test('simple js api', function(t) {
   var b = browserify();
   b.require(quadPath, { expose: 'quad' });
   b.transform(bubleify);
-  b.bundle((err, src) => {
+  b.bundle(function(err, src) {
     t.error(err);
     t.equal(runContextQuad5(src), 25);
     t.end();
@@ -32,7 +32,7 @@ test('simple js api', function(t) {
 test('simple js api with package.json', function(t) {
   var b = browserify();
   b.require(quadPkgPath, { expose: 'quad' });
-  b.bundle((err, src) => {
+  b.bundle(function(err, src) {
     t.error(err);
     t.equal(runContextQuad5(src), 25);
     t.end();
@@ -54,8 +54,10 @@ test('simple cli', function(t) {
     err += buf;
   });
 
-  bProcess.on('error', (processErr) => { throw processErr; });
-  bProcess.on('exit', () => {
+  bProcess.on('error', function onError(processErr) {
+    throw processErr;
+  });
+  bProcess.on('exit', function onExit() {
     t.error(err);
     t.equal(runContextQuad5(out), 25);
     t.end();
